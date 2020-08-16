0a6a09e246a37ef40908335caf766eaa41afe541
unknown <cjhongv@gmail.com>
2020-08-16 21:56:52 +1400

    final

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const Blog = require('../models/blog');
 
+// home
 router.get('/', (req, res) => {
-  res.render('index')
+  res.redirect('blogs')
+})
+//about
+router.get('/about', (req, res) => {
+  res.render('about', {title: 'about'})
+})
+//get all blogs
+router.get('/blogs', (req, res) => {
+  Blog.find().sort({createdAt: 'desc'})
+    .then(result => {
+      res.render('index', {title: 'blogs', blogs: result})
+    })
+    .catch(err => console.log(err))
+})
+// create blog
+router.get('/blogs/create', (req, res) => {
+  res.render('create', {title: 'create new blog'})
+})
+// create blog
+router.post('/blogs', (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    snippet: req.body.snippet,
+    body: req.body.body,
+  })
+  blog.save()
+    .then(result => {
+      res.redirect('/')
+    })
+    .catch(err => console.log(err))
+})
+//get single blog
+router.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then(result => {
+      res.render('details', {title: 'details', blog: result})
+    })
+    .catch(err => console.log(err))
+})
+//delete blog
+router.delete('/:id', (req, res) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err))
+})
+// edit blog  
+router.get('/edit/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then(result => {
+      res.render('edit', {title: 'edit', blog: result})
+    })
+    .catch(err => console.log(err))
+})
+//update blog
+// create blog
+router.put('/edit/:id', (req, res, next) => {
+  const blog = new Blog({
+    _id: req.params.id,
+    title: req.body.title,
+    snippet: req.body.snippet,
+    body: req.body.body,
+  });
+  Blog.updateOne({_id: req.params.id}, blog)
+  .then(result => {
+    res.redirect('/')
+  })
+  .catch(err => console.log(err))
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/indexRoute');
+const methodOverride = require('method-override')
 
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 mongoose.connect(process.env.DATABASE_URL, {
    useNewUrlParser: true, useUnifiedTopology: true,
@@ -22,5 +25,8 @@ db.once('open', () => console.log('connected to mongodb'));
 
 app.use(indexRouter);
 
+app.use((req, res) => {
+  res.render('404', {title: 'page not found'});
+})
 
 app.listen(process.env.PORT || 3000);
